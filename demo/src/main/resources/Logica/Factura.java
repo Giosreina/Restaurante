7307708d7d761e52d99ac73abd3cd177de0851e1
giosreina <giosreina@gmail.com>
2025-05-25 18:21:51 -0500

    Creación clases base factura, plato y comida

---
+++ demo/src/main/resources/Logica/Factura.java
@@ -0,0 +1,34 @@
+package Logica;
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Factura {
+    private int id;
+    private String cliente;
+    private String distribuidor;
+    private List<Plato> compras = new ArrayList<>();
+    private double precio;
+    private Date FECHA = new Date();
+    private String estado;
+
+    public Factura(int id, String cliente, String distribuidor, double precio, String estado) {
+        this.id = id;
+        this.cliente = cliente;
+        this.distribuidor = distribuidor;
+        this.precio = precio;
+        this.estado = estado;
+    }
+    public void addCompra(Plato plato) {
+        this.compras.add(plato);
+    }
+    public void removeCompra(Plato plato) {
+        this.compras.remove(plato);
+    }
+    public double getPrecio() {
+        for (Plato plato : compras) {
+            precio += plato.getPrecio();
+        }
+        return precio;
+    }
+}
