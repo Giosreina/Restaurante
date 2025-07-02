e29c1e14d085b73bca194a93283185151f66fcc5
giosreina <giosreina@gmail.com>
2025-07-01 22:15:18 -0500

    Iterador para mapas

--- demo/src/main/java/com/example/Patrones/Visitor/Almacen.java
+++ demo/src/main/java/com/example/Patrones/Visitor/Almacen.java
@@ -1,5 +1,37 @@
 package com.example.Patrones.Visitor;
 
-public class Almacen {
-    
+import com.example.Patrones.Iterator.*;
+import java.util.Map;
+
+public class Almacen extends Agregado{
+
+    private Map<String, Ingrediente> ingredientes;
+
+    public Iterator crearIterator(){
+        return new IteradorLista(this);
+    }
+
+    public int cantidadElementos(){
+        return ingredientes.size();
+    }
+    public Object obtenerElemento(int indice){
+        if (indice < 0 || indice >= ingredientes.size()) {
+            return null;
+        }
+        return ingredientes.values().toArray()[indice]; 
+    }
+
+    public void agregarElemento(Object comida){
+        if (comida instanceof Ingrediente) {
+            Ingrediente ingrediente = (Ingrediente) comida;
+            ingredientes.put(ingrediente.getNombre(), ingrediente);
+        }
+    }
+
+    public void eliminarElemento(Object comida){
+        if (comida instanceof Ingrediente) {
+            Ingrediente ingrediente = (Ingrediente) comida;
+            ingredientes.remove(ingrediente.getNombre());
+        }
+    }    
 }
